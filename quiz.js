65928515a99da12c2cacda6b64590aa21c06257d
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-08-06 09:08:38 +1200

    completed quiz

--- quiz.js
+++ quiz.js
@@ -29,11 +29,27 @@ const correctAnswers = [
 let usersAnswers = [];
 
 function askQuestion(num){
-  process.stdout.write(`${questions[num]}`);
+  process.stdout.write(`${questions[num]}\n`);
 }
 
 process.stdin.on('data', function(answer){
   let inputAnswer = answer.toString().trim();
+  let questionNum = usersAnswers.length;
+  if(inputAnswer === correctAnswers[questionNum]){
+    usersAnswers.push(inputAnswer);
+    if(usersAnswers.length === questions.length){
+      process.exit();
+    } else {
+      askQuestion(usersAnswers.length);
+    }
+  } else {
+    process.stdout.write(`WRONG!!!!, ${inputAnswer} is not the right answer, please try again\n`);
+  }
+
 });
 
 askQuestion(0);
+
+process.on('exit', function(){
+  process.stdout.write(`\nWell done, you got all the questions correct\n`);
+});
